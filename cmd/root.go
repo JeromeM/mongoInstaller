4b812cb210d07668d90ecd473865fdd1b870afed
Jerome <meyer.jerome@gmail.com>
2020-06-03 07:46:29 +0200

    Update descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -28,17 +28,17 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "mongo-installer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "mongoInstaller",
+	Short: "Download, install and configure any recent version of MongoDB",
+	Long: `Download, install and configure any recent version of MongoDB.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			_ = cmd.Help()
+			os.Exit(0)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -57,7 +57,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mongo-installer.yaml)")
+	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mongoInstaller.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -79,7 +79,7 @@ func initConfig() {
 
 		// Search config in home directory with name ".mongo-installer" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigName(".mongo-installer")
+		viper.SetConfigName(".mongoInstaller")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
